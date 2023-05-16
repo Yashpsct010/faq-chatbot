10db7b5dc8db015e71432cfafe75b4add90b9e31
Yash Parmar <yashpsct001@gmail.com>
2023-05-16 14:26:14 +0530

    faq

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                             userInput: userInput
                         },
                         success: function (response) {
-                            var chatbotReply = '<p class="chatbot-reply">' + response + '</p>';
+                            var chatbotReply = '<p id = "chatbot-reply" class="chatbot-reply">' + userInput +'<br>'+ response + '</p>';
                             $('#chatContainer').append(chatbotReply);
                         }
                     });
@@ -26,25 +26,44 @@
             });
         });
     </script>
+    <script>
+function clearContent(){
+    document.getElementById('faq');
+    document.body.style.width = '10px';
+    document.getElementById('chatContainer'&&'chatbot').innerHTML = '';
+
+}
+function openContent(){
+    document.getElementById('chatbot').innerHTML;
+    document.body.style.width = '10%';
+}
+    </script>
     <style>
         .chatbot-reply {
             color: white;
             
         }
         .close{
-            background-image:url('https://github.com/Yashpsct010/faq-chatbot/blob/master/cross-button.png');
             border-radius: 16px;
-            height:100px;
-            width:100px;
+            color:white;
+            background-size: contain;
+            background-color: blue;
+            float:right;
+            padding:2%;
+            width:25%;
         }
     </style>
 </head>
 <body>
-    <button class="close" id="close"></button>
-<div class="faq" style="color:white;border: 1px ridge purple;border-radius:16px 0px 16px 0px;background: linear-gradient(90deg, #234EB0 3.66%, #9010B0 100%);width:20rem;padding:2%;position: fixed;bottom: 2rem;right: 2rem;">
-    <h1>FAQ Chatbot</h1>
-        <div id="chatContainer"></div>
-        <input type="text" id="userInput" placeholder="Ask a question...">
-</div>   
+ 
+    <div class="faq" id="faq" style="color:white;border: 1px ridge purple;border-radius:16px 0px 16px 0px;background: linear-gradient(90deg, #234EB0 3.66%, #9010B0 100%);width:20rem;padding:2%;position: fixed;bottom: 2rem;right: 2rem;">
+        <input class="close" type="button" id="close" value="close" onclick="clearContent()">
+        <input class="close" type="button" id="open" value="open" onclick="openContent()">
+        <div class="chatbot" id="chatbot">
+        <h1>FAQ Chatbot</h1>
+            <div id="chatContainer"></div>
+            <input type="text" id="userInput" placeholder="Ask a question...">
+    </div>   
+    </div>   
 </body>
 </html>
